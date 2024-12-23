47ecbf286100cd1ba88494bc28a882fa269dbe95
Pratik <pratikshinde9957@gmail.com>
2024-12-23 17:54:35 +0530

    updated frontend urls

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
 
   const fetchTasks = async () => {
     try {
-      const response = await axios.get('http://localhost:5000/tasks');
+      const response = await axios.get('https://todobackend-8hg4.onrender.com/tasks');
       setTasks(response.data);
       
     } catch (err) {
@@ -28,7 +28,7 @@ function App() {
 
   const addTask = async () => {
     try {
-      const response = await axios.post('http://localhost:5000/tasks', { title: newTask });
+      const response = await axios.post('https://todobackend-8hg4.onrender.com/tasks', { title: newTask });
       setTasks([...tasks, response.data]);
       setNewTask('');
     } catch (err) {
@@ -38,7 +38,7 @@ function App() {
 
   const deleteTask = async (id) => {
     try {
-      await axios.delete(`http://localhost:5000/tasks/${id}`);
+      await axios.delete(`https://todobackend-8hg4.onrender.com/tasks/${id}`);
       setTasks(tasks.filter(task => task._id !== id));
     } catch (err) {
       console.error('Error deleting task:', err);
@@ -52,7 +52,7 @@ function App() {
 
   const saveEdit = async (id) => {
     try {
-      const response = await axios.put(`http://localhost:5000/tasks/${id}`, { title: editingTitle });
+      const response = await axios.put(`https://todobackend-8hg4.onrender.com/tasks/${id}`, { title: editingTitle });
       setTasks(tasks.map(task => (task._id === id ? response.data : task))); // Update the task in state
       cancelEdit(); // Exit editing mode
     } catch (err) {
